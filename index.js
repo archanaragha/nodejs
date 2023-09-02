e79605fe4c51e577c1b75850434e232fd34b2256
archanaragha <archanaaswath1994@gmail.com>
2023-09-02 22:51:20 +0530

    modified the message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.write("<h1>Hello</h1"));
+app.get("/", (req, res) => res.write("<h1>Hello Test</h1"));
 
 app.listen(3000, () => console.log("Port 3000"));
